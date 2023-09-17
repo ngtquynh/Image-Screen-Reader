f39480d11a5ff7db8d41e874643614c7046fd09d
ngtquynh <ntquynh.us@gmail.com>
2023-09-17 00:27:24 -0400

    m

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React, { useState } from "react";
 import Options from "./components/Options";
 import Input from "./components/Input";
@@ -6,7 +5,7 @@ import Output from "./components/Output";
 import "./App.css";
 
 function App() {
-  const [code, setCode] = useState("");
+  const [code, setCode] = useState(["", "", ""]); // Initialize code as an array
   const [selectedOptions, setSelectedOptions] = useState({
     language: "",
     framework: "",
@@ -19,13 +18,39 @@ function App() {
   const handleGenerateOutput = (inputText) => {
     // Check if both options are selected
     if (selectedOptions.language && selectedOptions.framework) {
-      // Combine the input text with selected options
-      const outputText = inputText + " " + selectedOptions.language + " " + selectedOptions.framework;
-      setCode(outputText);
+      // Update the code array with inputText, language, and framework
+      setCode([inputText, selectedOptions.language, selectedOptions.framework]);
+
+      // Send the code array to the backend
+      sendCodeToBackend([inputText, selectedOptions.language, selectedOptions.framework]);
     } else {
       // Display an error message if options are not selected
-      setCode("Please select both language and testing framework before generating output.");
+      setCode([
+        "Please select both language and testing framework before generating output.",
+        "",
+        "",
+      ]);
     }
+  };
+
+  const sendCodeToBackend = (codeArray) => {
+    // Send the code array to the Express backend
+    fetch("http://localhost:8000/message", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code: codeArray }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response from the backend if needed
+        console.log("Backend response:", data);
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error("Error sending data to backend:", error);
+      });
   };
 
   return (
@@ -41,7 +66,7 @@ function App() {
         </div>
         <div className="output-section">
           <h2>Generated Test Cases</h2>
-          <Output output={code} />
+          <Output output={code[0]} /> {/* Display the inputText from the code array */}
         </div>
       </div>
     </div>
